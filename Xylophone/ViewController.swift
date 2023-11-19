45a6b2a68c8a901ebbfad5c5149dab9434e3e9ea
Christopher Mena <you@chrismena.dev>
2023-11-18 21:13:12 -0500

    Linked all buttons and added playing sound func

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,16 +6,26 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
